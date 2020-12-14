67acf276728b27a10d95f9bd2e32f6ff12de5992
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2020-12-13 19:44:54 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!` 
+}
+
+function mondayWork(word="go to the office"){
+    return `This Monday, I will ${word}.`
+}
+
+function wrapAdjective(style="*"){
+    return function(message="a dedicated programmer"){
+        return `You are ${style}${message}${style}!`
+    }
+}
+   
+
+let Calculator = {
+    add: function(a, b){
+       return a + b
+    },
+
+    subtract: function(a, b){
+        return a - b
+    },
+
+    divide: function(a, b){
+        return a / b
+    }, 
+    
+    multiply: function(a, b){
+        return a * b
+    }
+}
+
+function actionApplyer(num, arrayOfFunctions){
+    for (let index = 0; index < arrayOfFunctions.length; index++) {
+        num = arrayOfFunctions[index](num) 
+    }
+    return num 
+} 
