942f5b5fbb174ef3d128a2ce10d0a0cf5ffc842b
wynterwolf <shinyguns@gmail.com>
2020-12-15 15:08:00 -0600

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Your code here
+function saturdayFun(action = "roller-skate") {
+    return `This Saturday, I want to ${action}!`
+}
+
+function mondayWork(work = "go to the office") { 
+    return `This Monday, I will ${work}.`
+}
+
+function wrapAdjective(flair="*") {
+    return function inner(adjective = "special") {
+        return `You are ${flair}${adjective}${flair}!`
+    }
+}
+
+var Calculator = {
+    add: (a, b) => a + b,
+    subtract: (a, b) => a - b,
+    multiply: (a, b) => a * b,
+    divide: (a, b) => a / b
+}
+
+function actionApplyer(starting, array) {
+    if (array === []) {
+        return starting
+    } else { 
+        for (let i = 0; i < array.length; i++)
+            starting = array[i](starting);
+    }
+    return starting;
+}
